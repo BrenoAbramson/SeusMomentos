ce9511ec769822ddcfd05bfe3ee4096bc7da89c8
Kaua <kauacarvalhokaua09@gmail.com>
2026-03-18 20:06:45 -0300

    Integração Front-end com Back-end

--- frontend/js/cadastro/script.js
+++ frontend/js/cadastro/script.js
@@ -47,13 +47,14 @@ form.addEventListener('submit', async (e) => {
     setLoading(true);
 
     try {
-        const response = await fetch('http://127.0.0.1:8080/api/login', { 
+        const response = await fetch('http://127.0.0.1:8000/users', { 
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
                 nome: nomeVal,
                 email: emailVal,
-                senha: senhaVal
+                senha: senhaVal,
+                confirmarsenha: confirmarSenhaVal
             })
         });
 
