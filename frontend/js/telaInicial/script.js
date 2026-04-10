b3f7c920fd731d12e8c846dcbd52557306c2a607
BrenoAbramson <brenoabramson0@gmail.com>
2026-04-10 19:04:28 -0300

    Ajustado todos os bugs

---
+++ frontend/js/telaInicial/script.js
@@ -0,0 +1,6 @@
+// Script da Tela Inicial
+document.addEventListener('DOMContentLoaded', () => {
+    // Código Javascript específico para a tela inicial 
+    // Console log de início
+    console.log("Tela Inicial carregada com sucesso.");
+});
